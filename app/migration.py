e8e7e0d4160ff2dc894c0795cb52ab42f7ecae19
Fredrik Welander <welandfr@arcada.fi>
2026-02-05 11:04:09 +0200

    add retry loop

--- app/migration.py
+++ app/migration.py
@@ -1,12 +1,17 @@
-import os, psycopg
+import os, time, psycopg
 from psycopg.rows import dict_row
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_conn():
-    return psycopg.connect(DATABASE_URL, 
-        autocommit=True, 
-        row_factory=dict_row)
+    for i in range(1, 6): # Retry loop...
+        try:
+            return psycopg.connect(DATABASE_URL, 
+                autocommit=True, 
+                row_factory=dict_row)
+        except:
+            print("WARNING: DB not ready, retrying...")
+            time.sleep(1)
 
 def migration():
     with get_conn() as conn, conn.cursor() as cur:
@@ -27,3 +32,4 @@ def migration():
                 ADD COLUMN IF NOT EXISTS updated_at TIMESTAMP DEFAULT now();
 
         """)
+        print("DB Migration DONE")
